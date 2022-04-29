fcf58f33c10c5bccee80574c2155331a771a0fb1
Orkhan Kazymov <rayskarken@gmail.com>
2022-04-30 01:02:13 +0300

    + design of Welcome page

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,27 @@
 <script setup>
 // import { RouterLink, RouterView } from 'vue-router'
 import WelcomeHeader from "./components/WelcomeHeader.vue";
+import WelcomeBody from "./components/WelcomeBody.vue";
 import {BContainer} from "bootstrap-vue-3";
 </script>
 
 <template>
   <b-container>
     <WelcomeHeader/>
+    <WelcomeBody/>
   </b-container>
   <RouterView />
 </template>
 
 <style>
 .color-custom-blue {
-  color: #285AB8;
+  background-color: #285AB8;
 }
 .color-custom-yellow {
   color: #FEFFA1;
+}
+.btn-custom-blue {
+  background-color: #285AB8;
+  border-color: #326cd2;
 }
 </style>
---
+++ src/components/WelcomeBody.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    </p>
+    <div class="centered">
+<!--   TODO add url to auth   -->
+      <b-button pill variant="primary" size="lg" class="btn-custom-blue font-monospace"><span>Join now</span></b-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WelcomeBody",
+  components: {}
+
+}
+</script>
+
+<style scoped>
+p {
+  margin-top: 1.5em;
+  font-family: monospace;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 3em;
+  line-height: 1.6em;
+  align-items: center;
+  text-align: center;
+}
+
+span {
+  margin: 0.5em;
+  font-size: 2.3em;
+  font-family: monospace;
+
+}
+
+.centered {
+  margin: 4em;
+  align-items: center;
+  text-align: center;
+}
+</style>
