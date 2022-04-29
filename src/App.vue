aebb5887239f6407b4aebd02c7d50a2d8d2aa614
Orkhan Kazymov <rayskarken@gmail.com>
2022-04-29 23:56:14 +0300

    renamed MainHeader to WelcomeHeader

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <script setup>
 // import { RouterLink, RouterView } from 'vue-router'
-import MainHeader from "./components/MainHeader.vue";
+import WelcomeHeader from "./components/WelcomeHeader.vue";
 import {BContainer} from "bootstrap-vue-3";
 </script>
 
 <template>
   <b-container>
-    <MainHeader/>
+    <WelcomeHeader/>
   </b-container>
   <RouterView />
 </template>
 
 <style>
-
+.color-custom-blue {
+  color: #285AB8;
+}
+.color-custom-yellow {
+  color: #FEFFA1;
+}
 </style>
--- src/components/WelcomeHeader.vue
+++ src/components/WelcomeHeader.vue
@@ -6,7 +6,7 @@
 
 <script>
 export default {
-  name: "MainHeader"
+  name: "WelcomeHeader"
 };
 </script>
 
