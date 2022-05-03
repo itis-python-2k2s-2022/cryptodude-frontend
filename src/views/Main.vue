fc246763f49c27336763c67ff4df747746f65881
Orkhan Kazymov <rayskarken@gmail.com>
2022-05-03 22:26:17 +0300

    + index page

---
+++ src/views/Main.vue
@@ -0,0 +1,15 @@
+<template>
+  <WelcomeHeader>This is main page. It's not finished yet...</WelcomeHeader>
+</template>
+
+<script>
+import WelcomeHeader from "../components/WelcomeHeader.vue";
+export default {
+  name: "Main",
+  components: {WelcomeHeader}
+}
+</script>
+
+<style scoped>
+
+</style>
