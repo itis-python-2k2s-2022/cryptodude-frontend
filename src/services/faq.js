438d4df10ccc30176d5c838d250564e54f59e8b3
Orkhan Kazymov <rayskarken@gmail.com>
2022-05-28 21:07:29 +0300

    + FAQs from server

---
+++ src/services/faq.js
@@ -0,0 +1,12 @@
+import request from "./api";
+
+export async function getFAQuestions() {
+    try {
+        const response = await request.get('/faq');
+        console.log(response.data);
+        return response.data;
+    } catch (e) {
+        console.log(e);
+        throw e;
+    }
+}
--- src/views/FAQ.vue
+++ src/views/FAQ.vue
@@ -1,24 +1,48 @@
 <template>
   <b-container>
     <WelcomeHeader>FAQ</WelcomeHeader>
-    <b-card-group>
-      <ul id="faq-list">
-        <li v-repeat="items">
-<!--          TODO-->
-          <FAQCard :question=""  :answer="{{ answer }}"/>
-        </li>
-      </ul>
-    </b-card-group>
-    <FAQCard question="Question?" answer="Answer!"/>
+    <p/>
+      <div v-if="isLoading"> Loading ... </div>
+      <div v-else-if="error"> {{ error.message }} <button @click="loadFAQ">try again</button> </div>
+      <div v-else>
+        <FAQCard
+          v-for="faquestion in fAQuestions"
+          :question="faquestion.question"
+          :answer="faquestion.answer"/>
+      </div>
   </b-container>
 </template>
 
 <script>
+import {ref} from "vue";
 import WelcomeHeader from "@/components/WelcomeHeader.vue";
 import FAQCard from "@/components/FAQCard.vue";
+import {getFAQuestions} from "../services/faq";
+
 export default {
   name: "FAQ",
-  components: { WelcomeHeader, FAQCard }
+  components: { WelcomeHeader, FAQCard },
+
+  data() {
+    const isLoading = ref(true);
+    const error = ref(null);
+    const fAQuestions = ref(null);
+    const loadFAQ = async () => {
+      isLoading.value = true;
+      try {
+        fAQuestions.value = await getFAQuestions();
+        isLoading.value = false;
+      } catch (e) {
+        error.value = e;
+      }
+    };
+
+    return { isLoading, error, fAQuestions, loadFAQ };
+  },
+  async mounted() {
+    await this.loadFAQ();
+  }
+
 }
 </script>
 
