e5a3be96426093487a3631f145916a9ef0fe3f01
Orkhan Kazymov <rayskarken@gmail.com>
2022-06-03 19:30:42 +0300

    + Hot Offers page

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
-export const BACKEND_HOST = import.meta.env.VITE_BACKEND_HOST;
-export const FRONTEND_FOR_BACKEND_HOST = import.meta.env
-  .VITE_FRONTEND_FOR_BACKEND_HOST;
+export const BACKEND_DOMAIN = import.meta.env.VITE_BACKEND_DOMAIN;
+export const BACKEND_PROTOCOL = import.meta.env.VITE_BACKEND_PROTOCOL
+export const BACKEND_HOST = `${BACKEND_PROTOCOL}://${BACKEND_DOMAIN}`
+export const FRONTEND_FOR_BACKEND_HOST = import.meta.env.VITE_FRONTEND_FOR_BACKEND_HOST;
 export const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
--- src/views/HotOffers.vue
+++ src/views/HotOffers.vue
@@ -1,13 +1,34 @@
 <template>
   <MainNavbar />
+  <p/>
+  <b-container>
+    <div v-for="offer in listOfHotPrices">
+      <OfferCard :offer="offer">
+        {{ offer.exchange }} - {{ offer.name_tag }} - {{ offer.price }} - {{ offer.change }}
+      </OfferCard>
+      <p/>
+    </div>
+  </b-container>
 </template>
 
 <script>
 import MainNavbar from "@/components/MainNavbar.vue";
+import {BACKEND_DOMAIN} from "../constants";
+import {ref} from "vue";
+import OfferCard from "@/components/OfferCard.vue";
 
 export default {
   name: "HotOffers",
-  components: { MainNavbar },
+  components: {OfferCard, MainNavbar },
+  data() {
+    const webSocket = new WebSocket(`ws://${BACKEND_DOMAIN}/crypto_api/hot_prices`, );
+    const listOfHotPrices = ref([]);
+    webSocket.onmessage = function (event) {
+      listOfHotPrices.value = JSON.parse(event.data);
+      console.log(listOfHotPrices);
+    }
+    return { webSocket, listOfHotPrices }
+  }
 };
 </script>
 
